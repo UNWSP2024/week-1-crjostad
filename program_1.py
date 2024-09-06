97effaaed394ce408d2fa6f2de7eb7a5d5d5856e
crjostad <crjostad@students.unwsp.edu>
2024-09-06 14:26:40 -0500

    Update program_1.py

--- program_1.py
+++ program_1.py
@@ -1,3 +1,4 @@
-def hello_world
-print("hello_world")
-
+def hello_world():
+    # Update the message below to say "Hello World", 
+    # Python cares about white space, so watch your indentation.
+    print("Hello World")
